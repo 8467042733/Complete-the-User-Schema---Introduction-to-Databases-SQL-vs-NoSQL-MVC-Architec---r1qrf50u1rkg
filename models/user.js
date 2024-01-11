de30fe27bf7cb791421f8db63f4b6ae940012e38
8467042733 <rupeshpal25112004@gmail.com>
2024-01-11 17:20:00 +0530

    add some code

--- models/user.js
+++ models/user.js
@@ -1,5 +1,24 @@
 var mongoose= require("mongoose");
 
 //Write your schema here with name of schema as userSchema
+const userSchema = mongoose.Schema({
+    name: {
+        type: string,
+        required: true,
+    },
+    email: {
+        type: string,
+        required: true,
+    },
+    pswd: {
+        type: string,
+        required: true,
+    },
+    role: {
+        enum: ['buyer','seller']
+    }
+});
+
+module.exports = mongoose.model("users",userSchema);
 
 module.exports =mongoose.model("users",userSchema);
